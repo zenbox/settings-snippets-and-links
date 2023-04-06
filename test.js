dd6f8a07b67709763764733ea5f60b6f71a3ec92
Michael Reichart <michael.reichart@gfu.net>
2023-04-06 05:25:00 +0200

    Update snippets

---
+++ test.js
@@ -0,0 +1,42 @@
+/*
+@author Michael
+*/
+
+/** {TM_FILENAME}
+ * @desc 
+*        
+ * @package   Webapplication
+ * @module    
+ * @author    Michael
+ * @since     2023/04/06
+ * @since     1.0.0
+ * @version   1.0.0
+ * @copyright 2023 Michael Reichart
+ * @license   CC 
+*/
+
+/** {test.js}
+ * @desc 
+*        
+ * @package   Webapplication
+ * @module    
+ * @author    Michael Reichart
+ * @since     2023/04/06
+ * @since     1.0.0
+ * @version   1.0.0
+ * @copyright 2023 Michael Reichart
+ * @license   CC 
+*/
+
+/** test.js
+ * @desc 
+*        
+ * @package   Webapplication
+ * @module    
+ * @author    Michael Reichart
+ * @since     2023/04/06
+ * @since     1.0.0
+ * @version   1.0.0
+ * @copyright 2023 Michael Reichart
+ * @license   CC 
+*/
